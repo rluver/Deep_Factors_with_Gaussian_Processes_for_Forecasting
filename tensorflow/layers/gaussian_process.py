60ace567f53128478fa3373728e4bbd442d2e338
rluver <skyd0608@gmail.com>
2022-06-28 23:28:13 +0900

    fix: Change gaussian process

--- tensorflow/layers/gaussian_process.py
+++ tensorflow/layers/gaussian_process.py
@@ -2,10 +2,10 @@ from ..kernels.kernels import SquaredExponentialKernel
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Layer
 
 
-class GaussianProcessRegressor(Model):
+class GaussianProcessRegressor(Layer):
     def __init__(self, sigma=0.5, kernel=SquaredExponentialKernel()):
         super(GaussianProcessRegressor, self).__init__()
         self.sigma = sigma
@@ -17,25 +17,22 @@ class GaussianProcessRegressor(Model):
         self.n = input_shape[0]
         self.identity = tf.eye(input_shape[0])
 
-    def call(self, inputs, targets):
+    def call(self, inputs, training=None):
         x = tf.broadcast_to(inputs, shape=(inputs.shape[0], inputs.shape[0]))
-        y = tf.broadcast_to(targets, shape=(targets.shape[0], targets.shape[0]))
         
         K = self._calculate_covariance_matrix(x, tf.transpose(x))
-        K_star = self._calculate_covariance_matrix(x, tf.transpose(y))
-        K_star2 = self._calculate_covariance_matrix(y, tf.transpose(y))
 
         cov_y = K + (self.sigma**2)*self.identity
         
         L = tf.linalg.cholesky(cov_y)
-        alpha = tf.transpose(L) / (L/targets)
-        f_star = (tf.transpose(K_star) @ alpha)[:, 0]
-        v = L / K_star
-        V_f_star = K_star2 - tf.transpose(v) @ v
+        alpha = tf.transpose(L) / (L/inputs)
+        f_star = (tf.transpose(K) @ alpha)[:, 0]
+        v = L / K
+        V_f_star = K - tf.transpose(v) @ v
 
-        _targets = targets[tf.newaxis, :]
+        _inputs = inputs[tf.newaxis, :]
         log_marginal_likelihood = (
-            -0.5 * tf.squeeze(_targets@tf.linalg.inv(cov_y)@tf.transpose(_targets)) 
+            -0.5 * tf.squeeze(_inputs@tf.linalg.inv(cov_y)@tf.transpose(_inputs)) 
             -0.5 * tf.math.log(tf.linalg.det(cov_y)) 
             -self.n/2 * tf.math.log(2*np.pi)
         )
